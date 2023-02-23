bc8fc69738f525c8c50f57c6dc19f3da12773817
Sickmantella <missilescout@gmail.com>
2023-02-22 18:40:38 -0700

    Mostly working balance

---
+++ src/main/java/frc/robot/commands/BalanceCmd.java
@@ -0,0 +1,82 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.commands;
+
+import edu.wpi.first.wpilibj2.command.CommandBase;
+import java.util.function.Supplier;
+import edu.wpi.first.math.controller.PIDController;
+import edu.wpi.first.math.filter.SlewRateLimiter;
+import edu.wpi.first.math.kinematics.ChassisSpeeds;
+import edu.wpi.first.math.kinematics.SwerveModuleState;
+import frc.robot.Constants.DriveConstants;
+import frc.robot.Constants.OIConstants;
+import frc.robot.subsystems.SwerveSubsystem;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
+public class BalanceCmd extends CommandBase {
+  private final SwerveSubsystem swerveSubsystem;
+  private final SlewRateLimiter xLimiter, yLimiter, turningLimiter;
+  private PIDController balancePID;
+
+  private final double balanceP = 0.045;
+  private final double balanceI = 0;
+  private final double balanceD = 0.013;
+
+  /** Creates a new BalanceCmd. */
+  public BalanceCmd(SwerveSubsystem swerveSubsystem) {
+    // Use addRequirements() here to declare subsystem dependencies.
+    this.xLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
+    this.yLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAccelerationUnitsPerSecond);
+    this.turningLimiter = new SlewRateLimiter(DriveConstants.kTeleDriveMaxAngularAccelerationUnitsPerSecond);
+    this.swerveSubsystem = swerveSubsystem;
+    this.balancePID = new PIDController(balanceP, balanceI, balanceD);
+    addRequirements(swerveSubsystem);
+    balancePID.setSetpoint(0);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    balancePID.reset();
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    float pitch = swerveSubsystem.getChassisPitch();
+    double xSpeed = balancePID.calculate(pitch);
+    SmartDashboard.putNumber("PID Output", xSpeed);
+    double ySpeed = 0;
+    double turningSpeed = 0;
+    // 3. Make the driving smoother
+    // xSpeed = xLimiter.calculate(xSpeed) * DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+    // ySpeed = yLimiter.calculate(ySpeed) * DriveConstants.kTeleDriveMaxSpeedMetersPerSecond;
+    // turningSpeed = turningLimiter.calculate(turningSpeed)
+    //     * DriveConstants.kTeleDriveMaxAngularSpeedRadiansPerSecond;
+
+    // 4. Construct desired chassis speeds
+    ChassisSpeeds chassisSpeeds;
+    chassisSpeeds = ChassisSpeeds.fromFieldRelativeSpeeds(
+        xSpeed, ySpeed, turningSpeed, swerveSubsystem.getRotation2d());
+
+    // 5. Convert chassis speeds to individual module states
+    SwerveModuleState[] moduleStates = DriveConstants.kDriveKinematics.toSwerveModuleStates(chassisSpeeds);
+    // 6. Output each module states to wheels
+    swerveSubsystem.setModuleStates(moduleStates, true);
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    swerveSubsystem.stopModules();
+    balancePID.reset();
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    return false;
+  }
+}
