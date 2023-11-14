894521725748e46553e7585c010b06fc684d846f
Burrit-o <sameorio@gmail.com>
2023-11-13 19:04:11 -0700

    Cleanup

--- src/main/java/frc/robot/commands/CubePickup.java
+++ src/main/java/frc/robot/commands/CubePickup.java
@@ -4,13 +4,9 @@
 
 package frc.robot.commands;
 
-import edu.wpi.first.wpilibj.XboxController;
 
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.Gripper;
-import frc.robot.Constants.GripperC;
-
 
 public class CubePickup extends CommandBase {
   private final Gripper m_subsystem;
